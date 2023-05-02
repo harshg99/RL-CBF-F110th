cefdba024b0d2a7c365c909c629f3aaeb1f6cccd
hydrargyrum5799 <hgoel1000p@gmail.com>
2023-05-02 01:51:35 -0400

    pure pursuit python script works. need to debug the max operation

--- pure_pursuit/scripts/pure_pursuit_node.py
+++ pure_pursuit/scripts/pure_pursuit_node.py
@@ -8,6 +8,8 @@ from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 from visualization_msgs.msg import Marker, MarkerArray
 import csv
 from scipy.spatial.transform import Rotation as R
+from nav_msgs.msg import Odometry
+from copy import deepcopy
 # TODO CHECK: include needed ROS msg type headers and libraries
 
 class PurePursuit(Node):
@@ -17,51 +19,54 @@ class PurePursuit(Node):
     """
     def __init__(self, filename = None):
         super().__init__('pure_pursuit_node')
-        self.declare_parameter('lookahead_distance', 1.0)
-        self.declare_parameter('velocity', 3.0)
-        self.declare_parameter('speed_lookahead_distance', 1.0)
+        self.declare_parameter('lookahead_distance', 1.2)
+        self.declare_parameter('velocity', 3.6)
+        self.declare_parameter('speed_lookahead_distance', 2.0)
         self.declare_parameter('brake_gain', 1.0)
         self.declare_parameter('wheel_base', 0.33)
-        self.declare_parameter('visualize', false)
+        self.declare_parameter('visualize', False)
         self.declare_parameter('curvature_thresh', 0.1)
         self.declare_parameter('acceleration_lookahead_distance', 5.0)
-        self.declare_parameter('accel_gain', 5.0)
+        self.declare_parameter('accel_gain', 0.0)
         
         # TODO: create ROS subscribers and publishers
-        self.lookahead_distance = self.get_parameter("lookahead_distance")
-        self.velocity = self.get_parameter("velocity").as_double()
-        self.speed_lookahead_distance = self.get_parameter("speed_lookahead_distance")
-        self.brake_gain = self.get_parameter("brake_gain")
-        self.wheel_base = self.get_parameter("wheel_base")
-        self.visualize = self.get_parameter("visualize")
-        self.curvature_thresh = self.get_parameter("curvature_thresh")
-        self.acceleration_lookahead_distance = self.get_parameter("acceleration_lookahead_distance")
-        self.accel_gain = self.get_parameter("accel_gain")
+        self.lookahead_distance = self.get_parameter("lookahead_distance").value
+        self.velocity = self.get_parameter("velocity").value
+        self.speed_lookahead_distance = self.get_parameter("speed_lookahead_distance").value
+        self.brake_gain = self.get_parameter("brake_gain").value
+        self.wheel_base = self.get_parameter("wheel_base").value
+        self.visualize = self.get_parameter("visualize").value
+        self.curvature_thresh = self.get_parameter("curvature_thresh").value
+        self.acceleration_lookahead_distance = self.get_parameter("acceleration_lookahead_distance").value
+        self.accel_gain = self.get_parameter("accel_gain").value
 
         self.pub_marker = self.create_publisher(MarkerArray, "marker_array", 10)
-        self.sub_pose = self.create_subscription(PoseStamped, "pf/viz/inferred_pose", self.pose_callback, 10)
+        #self.sub_pose = self.create_subscription(PoseStamped, "pf/viz/inferred_pose", self.pose_callback, 10)
+        self.sub_pose = self.create_subscription(Odometry, "/ego_racecar/odom", self.pose_callback, 10)
         self.pub_drive = self.create_publisher(AckermannDriveStamped, "drive", 10)
         
         if filename is None:
-            filename = "src/sparse_straights_interpolated.csv"
+            filename = "/sim_ws/src/pure_pursuit/src/sparse_straights_interpolated.csv"
     
         #load the csv file data and store the values in a numpy array
         
         positions = []
         with open(filename) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
+            print(csv_reader)
             line_count = 0
             for row in csv_reader:
                 if line_count == 0:
                     print(f'Column names are {", ".join(row)}')
                 else:
                     positions.append([float(row[0]), float(row[1])])
-                    line_count += 1
+                line_count += 1
         
         self.waypoints = np.array(positions)
+        print(f'Processed {line_count} lines.')
         
 
-    def compute_control(x,y,orientation):
+    def compute_control(self, x, y,orientation):
         pos = np.array([[x, y]])
         
         
@@ -69,18 +74,20 @@ class PurePursuit(Node):
         
         
         #lookahead for goals
-        lookahead_points = np.where(np.linalg.norm(self.waypoints - pos, axis=1) < self.lookahead_distance)
-        lookahead_points = lookahead_points[lookahead_points > closest_idx]
+        #print(self.lookahead_distance)
+        lookahead_points = np.where(np.linalg.norm(self.waypoints - pos, axis=1) < self.lookahead_distance)[0]
+        #print(lookahead_points)
+        lookahead_points = lookahead_points[np.where(lookahead_points > closest_idx)[0]]
         lookahead_idx = lookahead_points.max()
 
         #lookahead for seped reduction
-        speed_lookahead_points = np.where(np.linalg.norm(self.waypoints - pos, axis=1) < self.speed_lookahead_distance)
-        speed_lookahead_points = speed_lookahead_points[lookahead_points > lookahead_idx]
-        spoeed_lookahead_idx = speed_lookahead_points.max()
+        speed_lookahead_points = np.where(np.linalg.norm(self.waypoints - pos, axis=1) < self.speed_lookahead_distance)[0]
+        speed_lookahead_points = speed_lookahead_points[np.where(speed_lookahead_points > lookahead_idx)[0]]
+        speed_lookahead_idx = speed_lookahead_points.max()
 
         #lookahaead for acceleration
-        accel_lookahead_points = np.where(np.linalg.norm(self.waypoints - pos, axis=1) < self.accel_lookahead_distance)
-        accel_lookahead_points = accel_lookahead_points[lookahead_points > lookahead_idx]
+        accel_lookahead_points = np.where(np.linalg.norm(self.waypoints - pos, axis=1) < self.acceleration_lookahead_distance)[0]
+        accel_lookahead_points = accel_lookahead_points[np.where(accel_lookahead_points > speed_lookahead_idx)[0]]
         accel_lookahead_idx = accel_lookahead_points.max()
         
         # COmpute and transform all the ppoitns in the car's frame of reference
@@ -90,13 +97,21 @@ class PurePursuit(Node):
         
         orientation_q = deepcopy(orientation)
 
-        orientation_matrix = R.from_quat(orientation_q).inv().as_matrix()
+        orientation_matrix = R.from_quat(orientation_q).as_matrix()
+
+        so3_matrix = np.eye(4)
+        so3_matrix[0:3, 0:3] = orientation_matrix
+        so3_matrix[0:3, 3] = [x, y, 0]
+        so3_matrix = np.linalg.inv(so3_matrix)
+
         goal_point_4d = np.concatenate((goal_point, [0, 1]), axis = 0)
-        goal_car = np.matmul(orientation_matrix, goal_point_4d)
+        goal_car = np.matmul(so3_matrix, goal_point_4d)
+
         speed_goal_point_4d = np.concatenate((speed_goal_point, [0, 1]), axis = 0)
-        speed_goal_car = np.matmul(orientation_matrix, speed_goal_point_4d)
+        speed_goal_car = np.matmul(so3_matrix, speed_goal_point_4d)
+
         accel_goal_point_4d = np.concatenate((accel_goal_point, [0, 1]), axis = 0)
-        accel_goal_car = np.matmul(orientation_matrix, accel_goal_point_4d)
+        accel_goal_car = np.matmul(so3_matrix, accel_goal_point_4d)
 
         # compute the drive and velocities
         speed_heading = np.arctan2(speed_goal_car[1], speed_goal_car[0])
@@ -105,12 +120,13 @@ class PurePursuit(Node):
 
         lateral_displacement = goal_car[1]
         curvature = (2*lateral_displacement)/np.square(self.lookahead_distance)
-
+        
+        velocity = self.velocity - brake_amount
         if (abs(curvature) < self.curvature_thresh) and (abs(accel_heading) < 2*self.curvature_thresh):
             velocity += self.accel_gain*abs(2*self.curvature_thresh - abs(accel_heading))
         
-        velocity = velocity - brake_amount
-        steer = atan(self.wheel_base*curvature)
+       
+        steer = np.arctan(self.wheel_base*curvature)
         return velocity, steer
 
    
@@ -118,18 +134,18 @@ class PurePursuit(Node):
    # defines the controller for the      
     def pose_callback(self, pose_msg):
         # TODO: find the current waypoint to track using methods mentioned in lecture
-        x = pose_msg.pose.position.x
-        y = pose_msg.pose.position.y
-        orientation = [ pose_msg.pose.orientation.x, 
-                        pose_msg.pose.orientation.y, 
-                        pose_msg.pose.orientation.z, 
-                        pose_msg.pose.orientation.w]
+        x = pose_msg.pose.pose.position.x
+        y = pose_msg.pose.pose.position.y
+        orientation = [ pose_msg.pose.pose.orientation.x, 
+                        pose_msg.pose.pose.orientation.y, 
+                        pose_msg.pose.pose.orientation.z, 
+                        pose_msg.pose.pose.orientation.w]
         velocity, steer = self.compute_control(x,y,orientation)
         drive_msg = AckermannDriveStamped()
         drive_msg.header.frame_id = 'base_link'
-        drive_msg.drive.speed = velocity -  brake_amount
-        
-        pub_drive.publish(drive_msg)
+        drive_msg.drive.speed = velocity
+        drive_msg.drive.steering_angle = steer
+        self.pub_drive.publish(drive_msg)
         # TODO: publish drive message, don't forget to limit the steering angle.
 
 def main(args=None):
