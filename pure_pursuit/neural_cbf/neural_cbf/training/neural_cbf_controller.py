8c8b0494c72f9a5684ae046f9a710d94783c291c
hydrargyrum5799 <hgoel1000p@gmail.com>
2023-05-05 12:06:43 -0400

    changes made to policy gradient

--- pure_pursuit/neural_cbf/neural_cbf/training/neural_cbf_controller.py
+++ pure_pursuit/neural_cbf/neural_cbf/training/neural_cbf_controller.py
@@ -6,46 +6,14 @@ from torch import Tensor, nn
 from typing import Iterator, List, Tuple
 from torch.optim import Adam, Optimizer
 from neural_cbf.datamodules.episodic_datamodule import EpisodicDataModule
-from policy import Policy
 import torch.nn.functional as F
 from torch.autograd.functional import jacobian
 from collections import OrderedDict
-class CBFNet(nn.Module):
-    """Simple MLP network for computing V(x)"""
-    def __init__(self, n_dims: int, hidden_sizes: List[int] = [8]):
-        """
-        Args:
-            n_dims: input dimensionality
-            hidden_sizes: size of hidden layers
-        """ 
-        super().__init__()
-        self.layers: OrderedDict[str, nn.Module] = OrderedDict()
-        self.layers["input_linear"] = nn.Linear(
-            n_dims, hidden_sizes[0]
-        )
-        self.layers["input_activation"] = nn.ReLU()
-        
-        num_hidden_layer = len(hidden_sizes)
-        
-        for i in range(num_hidden_layer-1):
-            self.layers[f"layer_{i}_linear"] = nn.Linear(
-                hidden_sizes[i], hidden_sizes[i+1]
-            )
-            if i < num_hidden_layer - 1:
-                self.layers[f"layer_{i}_activation"] = nn.ReLU()
-        
-        self.layers["output_linear"] = nn.Linear(hidden_sizes[-1], 1)
-        
-        self.net = nn.Sequential(self.layers)
-    
-    
-    def forward(self, x):
-        V = self.net(x)
-        V = 0.5*(V*V).sum(dim=1)
-        return V
-
-
-
+from neural_cbf.datamodules.episodic_datamodule import (
+    EpisodicDataModule,
+)
+from neural_cbf.models import Policy, CBFNet
+from torch.optim.lr_scheduler import StepLR
 class NeuralCBFController(pl.LightningModule):
     """
     V(goal) = 0
@@ -54,7 +22,7 @@ class NeuralCBFController(pl.LightningModule):
     V(unsafe) > c
     dV/dt <= -lambda V
     """
-    def __init__(self, dynamics_model, datamodule, cbf_lambda=1.0, safe_level=1.0):
+    def __init__(self, dynamics_model, datamodule, system, cbf_lambda=1.0, safe_level=0.0):
         super().__init__()
         #self.save_hyperparameters()
         self.dynamics_model = dynamics_model
@@ -68,6 +36,14 @@ class NeuralCBFController(pl.LightningModule):
         self.safe_level = safe_level
         self.cbf_lambda = cbf_lambda
         
+        self.policy_loss_weight = 0
+        self.goal_loss_weight = 0
+        self.safe_loss_weight = 10
+        self.unsafe_loss_weight = 10 
+        self.descent_loss_weight = 100 
+        
+        # TODO need to remove later
+        self.dynamic_system = system 
     def descent_loss(
         self,
         x: torch.Tensor,
@@ -94,23 +70,16 @@ class NeuralCBFController(pl.LightningModule):
         # Compute loss to encourage satisfaction of the following conditions...
         loss = []
         eps = 0.1
-        #TODO modify this value, and don't put it here
-        a_3 = 10
-        JV = torch.eye(self.V.net[0].in_features) 
-        V = x
-        for layer in self.V.net:
-            V = layer(V)
-            if isinstance(layer, nn.Linear):
-                JV = torch.matmul(layer.weight, JV)
-            elif isinstance(layer, nn.ReLU):
-                JV = torch.matmul(torch.diag_embed(torch.sign(V)), JV)
-        
-        JV = torch.bmm(V.unsqueeze(1), JV).squeeze() 
-        #jacobian(self.V, x)
-        Lf_V = (JV * self.dynamics_model(x, self.policy_net(x))).sum(axis=1)
+        V, JV = self.V_with_jacobian(x)
+
+        #Detaching descent loss here
+        U = self.policy_net(x)
+        U.requires_grad = False
+        Lf_V = (JV * self.dynamics_model(x, U)).sum(axis=1)
         descent_violation = F.relu(eps + Lf_V + self.cbf_lambda*V.squeeze())
-        r = self.estimate_violation(batch)
-        cbf_descent_term = a_3*descent_violation.mean()  
+
+        #estimated_r = self.estimate_violation(x)
+        cbf_descent_term = self.descent_loss_weight*descent_violation.mean()  
         loss.append(("CBF descent term", cbf_descent_term))
         return loss
     
@@ -124,7 +93,9 @@ class NeuralCBFController(pl.LightningModule):
             elif isinstance(layer, nn.ReLU):
                 JV = torch.matmul(torch.diag_embed(torch.sign(V)), JV)
         
+       
         JV = torch.bmm(V.unsqueeze(1), JV).squeeze() 
+        #V = 0.5*(V*V)
        
         return V, JV 
     def boundary_loss(
@@ -155,13 +126,13 @@ class NeuralCBFController(pl.LightningModule):
         #   1.) CLBF should be minimized on the goal point
         goal_point = torch.zeros([1, self.dynamics_model.n_dims])
         V_goal_pt = self.V(goal_point)
-        goal_term = 1e1 * V_goal_pt.mean()
+        goal_term = self.goal_loss_weight* V_goal_pt.mean()
         loss.append(("CLBF goal term", goal_term))
 
         #   2.) 0 < V <= safe_level in the safe region
         V_safe = V[safe_mask]
         safe_violation = F.relu(eps + V_safe - self.safe_level)
-        safe_V_term = 1e2 * safe_violation.mean()
+        safe_V_term = self.safe_loss_weight * safe_violation.mean()
         loss.append(("CLBF safe region term", safe_V_term))
         if accuracy:
             safe_V_acc = (safe_violation <= eps).sum() / safe_violation.nelement()
@@ -170,7 +141,7 @@ class NeuralCBFController(pl.LightningModule):
         #   3.) V >= unsafe_level in the unsafe region
         V_unsafe = V[unsafe_mask]
         unsafe_violation = F.relu(eps + self.unsafe_level - V_unsafe)
-        unsafe_V_term = 1e2 * unsafe_violation.mean()
+        unsafe_V_term = self.unsafe_loss_weight * unsafe_violation.mean()
         loss.append(("CLBF unsafe region term", unsafe_V_term))
         if accuracy:
             unsafe_V_acc = (
@@ -181,10 +152,11 @@ class NeuralCBFController(pl.LightningModule):
         return loss
         
         
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch, batch_idx, optimizer_idx):
         """Conduct the training step for the given batch"""
         # Extract the input and masks from the batch
-        x, goal_mask, safe_mask, unsafe_mask = batch
+        print(optimizer_idx)
+        x, goal_mask, safe_mask, unsafe_mask, control = batch
 
         # Compute the losses
         component_losses = {}
@@ -202,13 +174,29 @@ class NeuralCBFController(pl.LightningModule):
             if not torch.isnan(loss_value):
                 total_loss += loss_value
 
-        batch_dict = {"loss": total_loss, **component_losses}
+        #batch_dict = {"loss": total_loss, **component_losses}
+        policy_loss = torch.nn.MSELoss()(self.policy_net(states[safe_mask]), control[safe_mask])  
+        
+        eps = 0.1
+        V, JV = self.V_with_jacobian(x)
 
-        return batch_dict
+        #Detaching descent loss here
+        U = self.policy_net(x)
+        V.requires_grad = False
+        JV.requires_grad = False
+        Lf_V = (JV * self.dynamics_model(x, U)).sum(axis=1)
+        descent_violation = F.relu(eps + Lf_V + self.cbf_lambda*V.squeeze())
+
+        total_loss +=(self.policy_loss_weight * policy_loss + self.descent_loss_weight*descent_violation.mean())
+        return total_loss
     
     def configure_optimizers(self) -> List[Optimizer]:
         """Initialize Adam optimizer."""
-        optimizer = Adam(self.V.parameters(), lr=1e-3)
+        V_optimizer = Adam(self.V.parameters(), lr=1e-3)
+        policy_optimizer = Adam(self.policy_net.parameters(), lr=1e-3)      
+        lr_scheduler = StepLR(V_optimizer, step_size=100, gamma=0.99)
+        return [V_optimizer, policy_optimizer], [lr_scheduler]
+    
         return optimizer
     
     def prepare_data(self):
@@ -218,16 +206,15 @@ class NeuralCBFController(pl.LightningModule):
     def train_dataloader(self):
         return self.datamodule.train_dataloader()
 
-    def estimate_violation(self, batch, n_samples=100): 
-        x, goal_mask, safe_mask, unsafe_mask = batch
+    def estimate_violation(self, x, n_samples=100): 
         batch_size = x.shape[0] 
         n_controls = self.dynamics_model.n_controls
+        n_dims = self.dynamics_model.n_dims
         upper_lim, lower_lim = self.dynamics_model.control_limits
         u_samples = torch.rand([batch_size * n_samples, n_controls])
         u_samples = u_samples*(upper_lim - lower_lim) + lower_lim
         V, JV = self.V_with_jacobian(x)
-        f_dot = self.dynamics_model(x.repeat_interleave(n_samples, dim=0), u_samples).view(batch_size, n_samples, 5)
+        f_dot = self.dynamics_model(x.repeat_interleave(n_samples, dim=0), u_samples).view(batch_size, n_samples, n_dims)
         lie_derivatives = torch.bmm(f_dot,JV.unsqueeze(2)).squeeze() 
         violation = (lie_derivatives + self.cbf_lambda*V).max(axis=1)[0]
         return F.relu(violation) 
-        
