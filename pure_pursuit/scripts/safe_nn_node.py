0fec04d4b617e8ce0e94d145019f32e74ea2b599
hydrargyrum5799 <hgoel1000p@gmail.com>
2023-05-09 11:13:16 -0400

    safe neural network node

--- pure_pursuit/scripts/safe_nn_node.py
+++ pure_pursuit/scripts/safe_nn_node.py
@@ -12,32 +12,65 @@ from nav_msgs.msg import Odometry
 from copy import deepcopy
 import argparse
 from pure_pursuit_control import PurePursuitController
-import tensorrt as trt
-import os
-import pycuda.driver as cuda
 import time
 import torch
 import argparse
+
+from neural_cbf.neural_cbf.datamodules import F110DataModule
+
+from argparse import ArgumentParser
+from scripts.create_data import F110System,parse_args
+
+from neural_cbf.neural_cbf.training import NeuralCBFController, F110DynamicsModel,ILController
+
 
 class NNController:
     def __init__(self, args):
 
         self.args = args
-        self.engine,self.context = self.build_engine(args.onnx_file_path, args)
-        if self.engine is None or self.context is None:
-            raise SystemExit('ERROR: failed to build the engine or context - check if model exists!')
-        for binding in self.engine:
-            if self.engine.binding_is_input(binding):  # we expect only one input
-                self.input_shape = self.engine.get_binding_shape(binding)
-                self.input_size = trt.volume(self.input_shape) * self.engine.max_batch_size * np.dtype(np.float32).itemsize  # in bytes
-                self.device_input = cuda.mem_alloc(self.input_size)
-            else:  # and one output
-                self.output_shape = self.engine.get_binding_shape(binding)
-                # create page-locked memory buffers (i.e. won't be swapped to disk)
-                self.host_output = cuda.pagelocked_empty(trt.volume(self.output_shape) * self.engine.max_batch_size, dtype=np.float32)
-                self.device_output = cuda.mem_alloc(self.host_output.nbytes)
+        if self.args.trt:
+            import tensorrt as trt
+            import os
+            import pycuda.driver as cuda
+
+            self.engine,self.context = self.build_engine(args.onnx_file_path, args)
+            if self.engine is None or self.context is None:
+                raise SystemExit('ERROR: failed to build the engine or context - check if model exists!')
+            for binding in self.engine:
+                if self.engine.binding_is_input(binding):  # we expect only one input
+                    self.input_shape = self.engine.get_binding_shape(binding)
+                    self.input_size = trt.volume(self.input_shape) * self.engine.max_batch_size * np.dtype(np.float32).itemsize  # in bytes
+                    self.device_input = cuda.mem_alloc(self.input_size)
+                else:  # and one output
+                    self.output_shape = self.engine.get_binding_shape(binding)
+                    # create page-locked memory buffers (i.e. won't be swapped to disk)
+                    self.host_output = cuda.pagelocked_empty(trt.volume(self.output_shape) * self.engine.max_batch_size, dtype=np.float32)
+                    self.device_output = cuda.mem_alloc(self.host_output.nbytes)
+        else:
+            datasource = F110DataModule(args,
+                                        model=F110System,
+                                        val_split=0.05,
+                                        batch_size=50,
+                                        quotas={"safe": 0.5, "unsafe": 0.4, "goal": 0.1},
+                                        )
+
+            dynamics = F110DynamicsModel(n_dims=5, n_controls=2)
+            system = F110System(args)
+            if self.args.il:
+                dir_path = "neural_cbf/training/checkpoints/" + args.version + "/model-v5.ckpt"
+                self.controller = ILController.load_from_checkpoint(dir_path, dynamics_model=dynamics,
+                                                                  datamodule=datasource, system=system)
+            else:
+                dir_path = "neural_cbf/training/checkpoints/" + args.version + "/model.ckpt"
+                self.controller = NeuralCBFController.load_from_checkpoint(dir_path, dynamics_model=dynamics,
+                                                                             datamodule=datasource, system=system)
+
 
     def build_engine(self, onnx_file_path, args):
+        import tensorrt as trt
+        import os
+        import pycuda.driver as cuda
+
         logger = trt.Logger(trt.Logger.WARNING)
         # initialize TensorRT engine and parse ONNX model
         engine_path = onnx_file_path.split('.')[0] + '16-{}'.format(args.fp16) + '.engine'
@@ -81,12 +114,13 @@ class NNController:
 
         return engine, context
 
-    def compute_control(self, state: np.ndarray):
+    def compute_control_trt(self, state: np.ndarray):
         '''
         state: [x, y, yaw, v, steer]
         Inputs a state and passes it through the NN to get the control
         '''
         # Create a stream in which to copy inputs/outputs and run inference.
+        import pycuda.driver as cuda
         stream = cuda.Stream()
         image_path = self.args.img_path + "/" + self.args.img_name
         # preprocess input data
@@ -100,6 +134,11 @@ class NNController:
         output_data = torch.Tensor(self.host_output).reshape(list(self.output_shape))
         control  = output_data.cpu().numpy().squeeze()
         return control[0],control[1]
+
+    def compute_control(self,state: np.ndarray):
+        control = self.controller.policy_net(state)
+        control = control.cpu().detach().squeeze().numpy()
+        return control
 
 class SafeNNControl(Node):
     """
@@ -119,12 +158,14 @@ class SafeNNControl(Node):
 
         self.controller = NNController(args)
         self.previous_control = np.array([0.0, 0.0])
+        self.args = args
 
     # defines the controller for the
     def pose_callback(self, pose_msg):
         # TODO: find the current waypoint to track using methods mentioned in lecture
         x = pose_msg.pose.pose.position.x
         y = pose_msg.pose.pose.position.y
+
         orientation = [pose_msg.pose.pose.orientation.x,
                        pose_msg.pose.pose.orientation.y,
                        pose_msg.pose.pose.orientation.z,
@@ -151,6 +192,9 @@ class SafeNNControl(Node):
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Safe NN Node')
+    parser.add_argument('--trt', type=bool, default=False, help='using trt or not')
+    parser.add_argument('--il', action='store_true', help='use CBF or IL')
+    parser.add_argument('--version', type=str, default='v0')
     parser.add_argument('--onnx_path', type=str, default='data/model.onnx', help='path to onnx model')
     parser.add_argument('--engine_path', type=str, default='data/model.trt', help='path to trt model')
     parser.add_argument('--fp16', action='store_true', help='inference with fp16')
