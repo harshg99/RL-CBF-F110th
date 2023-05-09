29ebd631e189f621d267aa10d8d105478a3ec3ce
hydrargyrum5799 <hgoel1000p@gmail.com>
2023-05-09 00:20:26 -0400

    Import issues resolved

--- pure_pursuit/neural_cbf/neural_cbf/training/__init__.py
+++ pure_pursuit/neural_cbf/neural_cbf/training/__init__.py
@@ -1,3 +1,4 @@
 from .neural_cbf_controller import NeuralCBFController
 from .train_dynamics import F110DynamicsModel
-__all__ = ["NeuralCBFController", "F110DynamicsModel"]
+from .il_controller import ILController
+__all__ = ["NeuralCBFController", "F110DynamicsModel", "ILController"]
